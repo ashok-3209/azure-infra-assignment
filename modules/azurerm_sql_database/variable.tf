ffaf2bf954a3cf5d1238299677c912c5bd764b9f
Ashok Kumar <ashok.kumar@gmail.com>
2025-12-20 23:10:16 +0530

    Fixed the Public IP & NSG Issue

--- modules/azurerm_sql_database/variable.tf
+++ modules/azurerm_sql_database/variable.tf
@@ -2,7 +2,7 @@ variable "sql_db_config" {
   type = map(object({
     sql_database_name = string
     sql_server_name  = string
-    # server_id         = string
+    server_id         = string
     resource_group_name = string
     location         = string
     collation         = string
